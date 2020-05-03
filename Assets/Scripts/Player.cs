7b87b280c594d05dac4b92fec97a808e6cfb194c
A8nton <toskagg01@gmail.com>
2020-05-03 22:16:05 +0300

    Player movement implemented

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private CharacterController _controller;
+    private Joystick _joystick;
+    private JumpButton _jumpButton;
+    private float _yVelocity;
+    [SerializeField]
+    private float _gravity = 1.0f;
+    [SerializeField]
+    private float speed = 5;
+    [SerializeField]
+    private float _jumpHeight = 25;
+
+    void Start() {
+        _controller = GetComponent<CharacterController>();
+        _joystick = FindObjectOfType<Joystick>();
+        _jumpButton = FindObjectOfType<JumpButton>();
+    }
+
+    void Update() {
+        CalculateMovement();
+    }
+
+    private void CalculateMovement() {
+        float horizontalInput;
+        bool isJumpPressed;
+    #if (UNITY_ANDROID || UNITY_IOS)
+        horizontalInput = _joystick.Horizontal;
+        isJumpPressed = _jumpButton.pressed;
+    #else
+        horizontalInput = Input.GetAxis("Horizontal");
+        isJumpPressed = Input.GetKeyDown(KeyCode.Space);
+    #endif
+        Vector3 direction = new Vector3(horizontalInput, 0, 0);
+        Vector3 velocity = direction * speed;
+
+        if (_controller.isGrounded) {
+            if (isJumpPressed) {
+                _yVelocity = _jumpHeight;
+            }
+        }
+        else {
+            _yVelocity -= _gravity;
+        }
+
+        velocity.y = _yVelocity;
+        
+        _controller.Move(velocity * (speed * Time.deltaTime));
+    }
+}
