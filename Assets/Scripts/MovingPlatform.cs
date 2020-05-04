e8f6a86f683fc0edaad960499499cd20e561b80f
A8nton <toskagg01@gmail.com>
2020-05-04 11:13:06 +0300

    Moving platform implemented

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+    [SerializeField]
+    private Transform _targetA, _targetB;
+    [SerializeField]
+    private float speed = 5;
+    private bool _moveRight = true;
+    
+    void Update() {
+        if (_moveRight) {
+            transform.position = Vector3.MoveTowards(transform.position, _targetB.position, Time.deltaTime * speed);
+            if (transform.position == _targetB.position) {
+                _moveRight = false;
+            }
+        }
+        else {
+            transform.position = Vector3.MoveTowards(transform.position, _targetA.position, Time.deltaTime * speed);
+            if (transform.position == _targetA.position) {
+                _moveRight = true;
+            }
+        }
+
+        {
+            
+        }
+    }
+}
