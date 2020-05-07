710f1cb2d189248673c50bd9f39a2ee08b17ba02
A8nton <toskagg01@gmail.com>
2020-05-07 09:32:15 +0300

    Respawn implemented

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour {
     private CharacterController _controller;
@@ -17,29 +17,39 @@ public class Player : MonoBehaviour {
     private float _jumpHeight = 25;
     [SerializeField]
     private int _coins;
-    
+    [SerializeField]
+    private int _lives = 3;
+    private Vector3 _initialPosition;
+    private const float _deadZone = -16f;
 
     void Start() {
         _controller = GetComponent<CharacterController>();
         _joystick = FindObjectOfType<Joystick>();
         _jumpButton = FindObjectOfType<JumpButton>();
         _uiManager = FindObjectOfType<UiManager>();
-    }
+        _uiManager.UpdateLives(_lives);
+        _uiManager.UpdateCoins(_coins);
+        _initialPosition = transform.position;
+        }
 
     void Update() {
         CalculateMovement();
-    }
+
+        if (transform.position.y < _deadZone) {
+            Respawn();
+            }
+        }
 
     private void CalculateMovement() {
         float horizontalInput;
         bool isJumpPressed;
-    #if (UNITY_ANDROID || UNITY_IOS)
+#if (UNITY_ANDROID || UNITY_IOS)
         horizontalInput = _joystick.Horizontal;
         isJumpPressed = _jumpButton.pressed;
-    #else
+#else
         horizontalInput = Input.GetAxis("Horizontal");
         isJumpPressed = Input.GetKeyDown(KeyCode.Space);
-    #endif
+#endif
         Vector3 direction = new Vector3(horizontalInput, 0, 0);
         Vector3 velocity = direction * speed;
 
@@ -47,23 +57,46 @@ public class Player : MonoBehaviour {
             if (isJumpPressed) {
                 _isDoubleJumpAllowed = true;
                 _yVelocity = _jumpHeight;
+                }
             }
-        }
         else {
             if (_isDoubleJumpAllowed && isJumpPressed) {
                 _yVelocity += _jumpHeight;
                 _isDoubleJumpAllowed = false;
-            }
+                }
+
             _yVelocity -= _gravity;
-        }
+
+            }
 
         velocity.y = _yVelocity;
-        
+
         _controller.Move(velocity * (speed * Time.deltaTime));
-    }
+        }
+
+    private void Respawn() {
+        _controller.enabled = false;
+        _lives--;
+
+        transform.position = _initialPosition;
+
+        if (_lives < 1) {
+            SceneManager.LoadScene(0);
+            _lives = 3;
+            }
+
+        _uiManager.UpdateLives(_lives);
+
+        StartCoroutine(RespawnRoutine());
+        }
+
+    IEnumerator RespawnRoutine() {
+        yield return new WaitForSeconds(0.1f);
+        _controller.enabled = true;
+        }
 
     public void UpdateCoins() {
         _coins++;
         _uiManager.UpdateCoins(_coins);
+        }
     }
-}
--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -6,9 +6,13 @@ using UnityEngine.UI;
 public class UiManager : MonoBehaviour {
     
     [SerializeField]
-    private Text _coinsText;
+    private Text _coinsText, _livesText;
 
     public void UpdateCoins(int coins) {
         _coinsText.text = "Coins: " + coins;
+    }
+
+    public void UpdateLives(int lives) {
+        _livesText.text = "Lives: " + lives;
     }
 }
